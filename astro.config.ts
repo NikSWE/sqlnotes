03e763a7df32438071c1901f0de1781832d79a05
Nikhil Anand <34477129+NikSWE@users.noreply.github.com>
2024-03-17 17:59:56 -0400

    update site theme

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      experimentalThemes: {
+        dark: "github-dark",
+        light: "github-light",
+      },
       wrap: true,
     },
   },
